dcfbc0d361b906f758f3b066aa9cb08718c7a27a
Kislak Serge <kislak7@gmail.com>
2011-11-08 15:05:48 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dragon::Application.config.secret_token = '154ef9e63468b02d86749015aabffa2415e2ce2b6b79fee0aaeb9c62c8fa2130441811bf438427b922b7a8cd274019e1ee6e93c02c7acd14b5dcd373fea7b86b'
